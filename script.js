5d8d10a124e8522a350b4b2229ea8f8ce311b9ba
sachintha999 <sachinthadil53383@gmail.com>
2023-11-11 22:59:09 +0530

    enhance features

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 // typed js 
 
 var typed = new Typed(".multiple-text", {
-    strings: ["Frontend Developer", "Wordpress Developer", "Blogger", "Graphic Designer"],
+    strings: ["Frontend Developer", "Wordpress Developer", "Blogger", "Graphic Designer", "Mobile App Developer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
